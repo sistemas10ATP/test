e34ae1085684a1f5f04e8186dbbae8c7a58d3d3d
sistemas10 <sistemas10@avanceytec.com.mx>
2018-09-05 11:26:47 -0600

    login

--- application/assets/js/views/login.js
+++ application/assets/js/views/login.js
@@ -6,6 +6,22 @@
 
 
 function send(){
-    var data=$("#login").serialize();
-    console.log(data);
+    var dataForm=$("#login").serialize();
+    $.ajax({
+        url:"http://intranet.enlaceatp.net/ldap",
+        data: dataForm,
+        dataType: 'json',
+        type: 'POST',
+        beforeSend: function (xhr) {
+            var img='<img src="../application/assets/img/cargando.gif" style="width: 0.5em;">';
+            $('#result').html(img);
+        },
+        success: function (data, textStatus, jqXHR) {
+            $('#result').html("");
+            console.log(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            $('#result').html("");
+        }
+    });
 }

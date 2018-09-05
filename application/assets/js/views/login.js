fdfd69e583e00792dcc0dd469064fdc687fec3ba
sistemas10 <sistemas10@avanceytec.com.mx>
2018-09-05 12:06:58 -0600

    login

--- application/assets/js/views/login.js
+++ application/assets/js/views/login.js
@@ -28,7 +28,7 @@ function send(){
                 localStorage.setItem("nomuser", data.nomuser);
                 localStorage.setItem("user", data.user);
                 localStorage.setItem("username", data.username);
-                window.location.href = "/";                
+                window.location.href = "/inax";                
             }
         },
         error: function (jqXHR, textStatus, errorThrown) {
